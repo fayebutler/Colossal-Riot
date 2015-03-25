f5d9b5efc2d4fc78ededb626c528874f64355681
fayebutler <i7715430@bournemouth.ac.uk>
2015-03-25 17:23:38 +0000

    steering updates

--- RiotGame/include/GameWorld.h
+++ RiotGame/include/GameWorld.h
@@ -1,24 +1,39 @@
 #ifndef GAMEWORLD_H
 #define GAMEWORLD_H
 
-#include
+
 #include "BaseGameEntity.h"
 //#include "cellSpacePartition"
 #include <ngl/Transformation.h>
 
+#include "Vehicle.h"
+#include <vector>
 //class
 //
 //
 
+//typedef std::vector<BaseGameEntity*>::iterator ObIt;
 
 class GameWorld
 {
 private:
-    //std::vector of vehicles
-
-    //any obstacles
     std::vector<BaseGameEntity*> m_obstacles;
 
+    std::vector<Vehicle*> m_vehicles;
+
+    ngl::Vec3 m_crosshair;
+
+public:
+    GameWorld();
+    ~GameWorld();
+
+    ngl::Vec3 Crosshair()const{return m_crosshair;}
+    void setCrosshair(ngl::Vec3 v){m_crosshair=v;}
+
+    void Update(double timeElapsed);
+    void Render();
+
+    const std::vector<Vehicle*>& Agents(){return m_vehicles;}
 
     //std::vector<Wall2D> m_Walls;
 };
--- RiotGame/src/GameWorld.cpp
+++ RiotGame/src/GameWorld.cpp
@@ -1,1 +1,16 @@
 
+#include "GameWorld.h"
+
+
+
+GameWorld::GameWorld()
+{
+   ngl::Vec3 pos = ngl::Vec3(0,0,0);
+   Vehicle* vehicle = new Vehicle(this,pos, ngl::Vec3(0,0,0), 0.0, 0.0, 10.0, 10.0,10.0,1.0);
+   vehicle->Steering()->ArriveOn();
+   vehicle->Steering()->FleeOn();
+   vehicle->Steering()->SeekOn();
+
+   m_vehicles.push_back(vehicle);
+
+}
